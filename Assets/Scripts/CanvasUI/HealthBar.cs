74de4293c6cf812aa2d77785318ed5645cf239f3
maucamsan <47923785+maucamsan@users.noreply.github.com>
2022-11-10 17:31:43 -0500

    Add Hunger Bar Behavior

--- Assets/Scripts/CanvasUI/HealthBar.cs
+++ Assets/Scripts/CanvasUI/HealthBar.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+[RequireComponent(typeof(Slider))]
 public class HealthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Slider hungerMeter;
+    public float hunger = 100;
+    Color fullColor;
+    Color hungryColor;
+    Color reference;
+    [SerializeField] Image imageFiller;
     void Start()
     {
-        
+        hungerMeter = GetComponent<Slider>();
+        reference = GetComponentInChildren<Image>().color;
+        fullColor = reference;
+        hungryColor = Color.red;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleColorBar();
+    }
+    void HandleColorBar()
+    {
+        Debug.Log(hungerMeter.value);
+        float t = Mathf.InverseLerp(20f, 50f, hungerMeter.value);
+        imageFiller.color = Color.Lerp(hungryColor, fullColor, t);
     }
 }
